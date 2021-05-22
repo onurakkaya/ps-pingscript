c383a71f4c9dad2e3805a3e06f19379d96a05a11
Onur AKKAYA <onurakkaya26@hotmail.com>
2021-05-22 17:47:27 +0300

    Initial Commit

---
+++ pingscript.ps1
@@ -0,0 +1,36 @@
+﻿$textFile = Get-Content .\intra.txt
+
+$counter = 0 
+$resultArray =  New-Object 'object[]' $textFile.Length
+foreach ($line in $textFile)
+{
+    $counter = $counter + 1
+    $urlArr = $line.Split("/")
+    $url = $null
+    if ($urlArr.Length -gt 0)
+    {
+        $url = $urlArr[0];
+    }
+    else
+    {
+        $url = $line
+    }
+
+    
+    Write-Host Raw Line: $counter $line, Splitted Url: $url, Server Response:  $responseAddress
+    $results=  (Test-NetConnection -InformationLevel Detailed -ComputerName $url)
+    $responseAddress = $results.RemoteAddress
+
+    if ($responseAddress.ToString().Length -gt 0)
+    {
+        $resultArray[$counter] = $responseAddress.ToString()
+    }
+    else
+    {
+        $resultArray[$counter] = " "
+    }
+
+    
+}
+
+$resultArray | Out-File .\ping-response.txt
